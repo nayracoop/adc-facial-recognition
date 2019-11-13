28ff0fdb604a3e0328314e1db516d0695d6c7339
Ignacio <ignacio.guerra@nayra.coop>
2019-11-13 16:51:08 -0300

    Counter component

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Intro.scss'
 import { Container, Row, Col, Jumbotron } from 'react-bootstrap'
-import MatchLevel from '../common/MatchLevel/MatchLevel'
+import Counter from '../common/Counter/Counter'
 
 const Intro = () => {
   return (
@@ -11,7 +11,7 @@ const Intro = () => {
           <Col xs={{ span: 9, offset: 2 }} sm={{ span: 9, offset: 2 }} lg={{ span: 5, offset: 3 }}>
             <h1>ConMiCaraNo</h1>
             <p>Reconocimiento facial en la <br />Ciudad de Buenos Aires</p>
-            <MatchLevel max="89" units="%" />
+            <Counter max="89" units="%" />
           </Col>
         </Row>
       </Container>
