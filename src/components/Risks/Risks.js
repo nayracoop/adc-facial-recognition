b28618b6dc87659aaaef4c51e5c19bf39b363523
Ignacio <ignacio.guerra@nayra.coop>
2019-11-19 10:39:04 -0300

    Visibility Sensor functionality implemented as demo in Risks

--- src/components/Risks/Risks.js
+++ src/components/Risks/Risks.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import './Risks.scss'
 import {Container, Row, Col} from 'react-bootstrap'
+import VisibilitySensor from 'react-visibility-sensor'
+
+import Counter from '../common/Counter/Counter'
+
 
 const Risks = (props) => {
   const risksList = props.riskCardsList.map(item => <Row className='justify-content-center'><Col lg={5} className='mb-5'>{item}</Col></Row>)
   return (
-    <section id='risks'>
-      <Container fluid>
-        <h2>Riesgos</h2>
-        {risksList}
-      </Container>
-    </section>
+    <VisibilitySensor partialVisibility={true}>
+      {({isVisible}) => 
+        <section id='risks'>
+          <Container fluid>
+            <h2>Riesgos {isVisible && <Counter max="89" units="%" />}</h2>
+            {risksList}
+          </Container>
+        </section>
+      }
+    </VisibilitySensor>
   )
 }
 
