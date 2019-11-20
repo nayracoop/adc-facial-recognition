740c70954702c334ecec3b62e548d223584f8ada
Ignacio <ignacio.guerra@nayra.coop>
2019-11-20 20:26:19 -0300

    Mobile scroll behavior asociated with scroll

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,43 @@ import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap'
 import Logo from '../../assets/img/logo.svg'
 import Cam from '../../assets/img/cam.svg'
 
-const Header = (props) => {
-  function openHeader (expanded) {
+let lastScrollY = 0;
+
+class Header extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      scrolled: false,
+      scrolledUp: false,
+      opened: false,
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll, true);
+    this.handleScroll();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+  
+  handleScroll = () => {
+    const isScrolledUp = lastScrollY - window.scrollY > 0;
+    const isScrolled = window.scrollY > 0;
+    lastScrollY = window.scrollY;
+    if(isScrolled !== this.state.scrolled || isScrolledUp !== this.state.scrolledUp) {
+      this.setState({ scrolled: isScrolled, scrolledUp: isScrolledUp });
+    }
+  }
+  
+  openHeader = (expanded) => {
+    this.setState({ opened: expanded });
+
     if (expanded) {
       //document.body.style.overflow = 'hidden'
+      this.setState({ opened: expanded });
       document.querySelector('.header-cam-cont').classList.toggle('rotate')
     } else {
       //document.body.style.overflow = 'auto'
@@ -35,39 +68,46 @@ const Header = (props) => {
       console.log('abierto')
     }
   pepito() */
-  return (
-    <Container fluid className='header-container'>
-      <Row>
-        <Col lg={{ span: 10, offset: 1 }}>
-          <header>
-            <Navbar expand='lg' onToggle={openHeader} collapseOnSelect={true}>
-              <Navbar.Brand href='#'>
-                <span className='sr-only'>ADC por los derechos civiles</span>
-                <img src={Logo} height='75' alt='ADC por los derechos civiles' />
-              </Navbar.Brand>
-              <Navbar.Toggle aria-controls='basic-navbar-nav' className='ml-auto' />
-              <Navbar.Collapse id='basic-navbar-nav'>
-                <Nav className='ml-auto'>
-                  <Nav.Link href='#working'>¿cómo funciona?</Nav.Link>
-                  <Nav.Link href='#risks'>riesgos</Nav.Link>
-                  <Nav.Link href='#about-adc'>¿qué está haciendo la ADC?</Nav.Link>
-                </Nav>
-                <div className='header-cam-cont mob-only'>
-                  <img src={Cam} height='67' alt='Cam' className='header-cam' />
-                </div>
-                <div className='text-center header-social mob-only'>
-                  <h3 className='mb-4'>Redes ADC</h3>
-                  <div className='d-flex justify-content-around social-cont m-auto'>
-                    {props.socialList}
+
+  render () {
+
+    let classes = this.state.scrolled && !this.state.scrolledUp && !this.state.opened ? 'hidden' : '';
+    if(this.state.opened) classes = 'opened';
+
+    return (
+      <Container fluid className='header-container'>
+        <Row>
+          <Col lg={{ span: 10, offset: 1 }}>
+            <header className={classes}>
+              <Navbar expand='lg' onToggle={this.openHeader} collapseOnSelect={true}>
+                <Navbar.Brand href='#'>
+                  <span className='sr-only'>ADC por los derechos civiles</span>
+                  <img src={Logo} height='75' alt='ADC por los derechos civiles' />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls='basic-navbar-nav' className='ml-auto' />
+                <Navbar.Collapse id='basic-navbar-nav'>
+                  <Nav className='ml-auto'>
+                    <Nav.Link href='#working'>¿cómo funciona?</Nav.Link>
+                    <Nav.Link href='#risks'>riesgos</Nav.Link>
+                    <Nav.Link href='#about-adc'>¿qué está haciendo la ADC?</Nav.Link>
+                  </Nav>
+                  <div className='header-cam-cont mob-only'>
+                    <img src={Cam} height='67' alt='Cam' className='header-cam' />
                   </div>
-                </div>
-              </Navbar.Collapse>
-            </Navbar>
-          </header>
-        </Col>
-      </Row>
-    </Container>
-  )
+                  <div className='text-center header-social mob-only'>
+                    <h3 className='mb-4'>Redes ADC</h3>
+                    <div className='d-flex justify-content-around social-cont m-auto'>
+                      {this.props.socialList}
+                    </div>
+                  </div>
+                </Navbar.Collapse>
+              </Navbar>
+            </header>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
 }
 
 export default Header
